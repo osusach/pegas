c8cb296b154de66f9d79a0126825cca84b0264fa
Skyrdow <lucas.mesias@usach.cl>
2024-01-19 22:28:50 -0300

    add font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,14 @@
 <script>
   import "../app.css";
+  import '@fontsource-variable/inter';
 </script>
 
 <div class="h-screen w-screen bg-base-100 text-gray-100">
   <slot class="h-full items-center justify-center" />
 </div>
+
+<style>
+  :global(body) {
+    font-family: 'Inter Variable', sans-serif;
+  }
+</style>
